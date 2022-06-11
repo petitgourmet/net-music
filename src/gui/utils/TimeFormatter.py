aa772946ca6177ccde4a20de0b7110d2de842b06
Fengwei Yu <jeremy.yu@outlook.fr>
2022-06-11 18:22:37 +0200

    Create TimeFormatter.py

---
+++ src/gui/utils/TimeFormatter.py
@@ -0,0 +1,27 @@
+# 时间转换
+import math
+import time
+from datetime import datetime
+
+
+def MSToTime(ms):
+    return SToTime(ms / 1000)
+
+
+def SToTime(s):
+    minute = int(s // 60)
+    if minute < 10:
+        minute = '0' + str(minute)
+    second = math.floor(s % 60)
+    if second < 10:
+        second = '0' + str(second)
+    return str(minute) + ":" + str(second)
+
+
+def getTimeStamp():
+    return math.floor(time.time())
+
+
+def timeStamptoTime(timeStamp):
+    return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(timeStamp))
+
